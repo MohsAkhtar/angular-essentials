10ed1b72411e1dc3270e5c9150c1616651baf877
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-10 22:19:06 +0000

    Added service injection into another service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { TabsComponent } from './tabs/tabs.component';
 import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 import { StarWarsService } from './star-wars.service'; // injecting here gives one instance of dependency for whole app
+import { LogService } from './log.service';
 
+// services can only recieve other services as dependencies if you provide them in the app module
 @NgModule({
   declarations: [AppComponent, TabsComponent, ListComponent, ItemComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [StarWarsService],
+  providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,8 +1,22 @@
+import { LogService } from './log.service';
+import { Injectable } from '@angular/core';
+
+// if you inject a service into another service, you need to give that service where you want to inject into
+// (So where you set up the dependency in the constructor) you have to add the decorator @Injectable() to that service.
+// This is required because Angulars dependency injector only works with classes which have a decorator.
+// This decorator does not do much but it allows Angular to inject the service into our service.
+@Injectable()
 export class StarWarsService {
   private characters = [
     { name: 'Luke Skywalker', side: '' },
     { name: 'Darth Vader', side: '' }
   ];
+
+  private logService: LogService;
+
+  constructor(logService: LogService) {
+    this.logService = logService;
+  }
 
   getCharacters(chosenList) {
     if (chosenList === 'all') {
@@ -19,5 +33,8 @@ export class StarWarsService {
       return char.name === charInfo.name;
     });
     this.characters[pos].side = charInfo.side;
+    this.logService.writeLog(
+      'Changed side of ' + charInfo.name + ', new side: ' + charInfo.side
+    );
   }
 }
